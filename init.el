4cdff8b19c052dfe568b6a010e2dbf6eef18b353
but0n <but0n@icloud.com>
2016-10-05 18:57:24 +0800

    [MOD]: Set Font Size to 14

--- init.el
+++ init.el
@@ -50,6 +50,8 @@
 ;; Disable audio bell
 (setq ring-bell-function 'ignore)
 
+;; Set Default Font Size
+(set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
 
 ;; =_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=
 
@@ -64,7 +66,9 @@
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
    (quote
-    ("a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" default))))
+    ("a800120841da457aa2f86b98fb9fd8df8ba682cebde033d7dbf8077c1b7d677a" default)))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
